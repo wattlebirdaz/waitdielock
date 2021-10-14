f872397eca678190062e17df40cf44bffa1a4c0a
wattlebirdaz_optiplex <wattlebirdaz@gmail.com>
2021-10-14 14:43:16 +0900

    added tests

--- main.cpp
+++ main.cpp
@@ -223,6 +223,39 @@ void test9()
     wd.trace();
 }
 
+void test10()
+{
+    // multiple threads - write after several writes and reads
+    printf("Multiple threads - write after several writes and reads\n");
+
+    std::vector<std::thread> threads;
+    int num_threads = 10;
+    threads.reserve(num_threads);
+
+    WaitDieLock wd;
+    threads.emplace_back(run_write, 10, 10, 1, std::ref(wd));
+    threads[0].join();
+    wd.trace();
+    threads.emplace_back(run_read, 9, 9, 1, std::ref(wd));
+    threads.emplace_back(run_write, 8, 8, 1, std::ref(wd));
+    threads.emplace_back(run_read, 7, 7, 1, std::ref(wd));
+    threads.emplace_back(run_read, 6, 6, 1, std::ref(wd));
+    wd.trace();
+    threads.emplace_back(run_unlock, 10, 10, 1, std::ref(wd));
+    threads[1].join();
+    threads[5].join();
+    wd.trace();
+    threads.emplace_back(run_unlock_shared, 9, 9, 1, std::ref(wd));
+    threads[2].join();
+    threads[6].join();
+    wd.trace();
+    threads.emplace_back(run_unlock, 8, 8, 1, std::ref(wd));
+    threads[3].join();
+    threads[4].join();
+    threads[7].join();
+    wd.trace();
+}
+
 int main()
 {
     test1();
@@ -234,4 +267,5 @@ int main()
     test7();
     test8();
     test9();
+    test10();
 }
